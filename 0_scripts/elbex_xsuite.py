76f6ccbb69094ae41a1ab4cdad045e0cb8f3eab2
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-02-23 16:39:58 +0000

    Magnet moves for shielding

--- 0_scripts/elbex_xsuite.py
+++ 0_scripts/elbex_xsuite.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as _plt 
+import pandas as _pd
 
 def plotTwissXYAlongS(tws, line = 'XTD8', y1='betx', y2='bety', label=r'\beta', unit='m', factor=1,
                       xlim=None, ylim=None, bdsimpath=None, figsize=[12, 3]):
@@ -14,3 +15,62 @@ def plotTwissXYAlongS(tws, line = 'XTD8', y1='betx', y2='bety', label=r'\beta', 
     _plt.ylabel(r'${}$ [{}]'.format(label, unit))
     _plt.xlabel(r'$s$ [m]')
     _plt.grid(True)
+
+def writeXsuiteToExcel(xsuite, outputfilename, scut=None):
+    df = buildDF(xsuite, scut=scut)
+    df.to_excel(outputfilename)
+
+
+def buildDF(xsuite, scut=None):
+    survey = xsuite.env.XTD8.survey(**xsuite.surv0)
+
+    df = _pd.DataFrame()
+    for coord in ['name', 'X', 'Y', 'Z', 'theta', 'phi', 'psi', 'angle']:
+        df[coord] = getattr(survey, coord)
+    T = []
+    for t in survey.element_type:
+        if t == 'Drift':
+            T.append('DRIF')
+        elif t == 'Marker' or t == '':
+            T.append('MARK')
+        elif t == 'RBend':
+            T.append('RBEN')
+        elif t == 'SBend' or t == 'Bend':
+            T.append('SBEN')
+        elif t == 'Quadrupole':
+            T.append('QUAD')
+        elif t == 'Sextupole':
+            T.append('SEXT')
+        else:
+            print('type : ', t)
+    df['TYPE'] = T
+    df['L'] = survey.length
+
+    df_mag = df[df.TYPE.isin(['RBEN', 'SBEN', 'QUAD', 'SEXT', 'MARK'])]
+
+    CLASS = []
+    for name in df_mag.name:
+        CLASS.append(name.split('.')[0])
+    df_mag['CLASS'] = CLASS
+
+    if scut is not None:
+        df_mag = df_mag[df_mag.s >= scut]
+
+    K1 = []
+    for name in df_mag.name:
+        try:
+            K1.append(xsuite.env[name].k1)
+        except:
+            K1.append(0)
+    df_mag['K1'] = K1
+    df_mag['STRENGH'] = df_mag['K1'] * df_mag['L']
+
+    df_mag = df_mag.rename(columns={'name': 'NAME'})
+    df_mag = df_mag.rename(columns={'angle': 'ANGLE'})
+    df_mag = df_mag.rename(columns={'theta': 'THETA'})
+    df_mag = df_mag.rename(columns={'phi': 'PHI'})
+    df_mag = df_mag.rename(columns={'psi': 'PSI'})
+
+    df_mag = df_mag[['NAME', 'TYPE', 'CLASS', 'X', 'Y', 'Z', 'THETA', 'PHI', 'PSI', 'L', 'ANGLE', 'K1', 'STRENGH']]
+
+    return df_mag
